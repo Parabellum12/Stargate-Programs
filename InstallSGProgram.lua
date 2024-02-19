e3ed8bfd8a4ab92f2fea5c14f0c6196d59821960
Parabellum12 <amessmer@themessmers.net>
2024-02-19 14:15:55 -0800

    t

--- InstallSGProgram.lua
+++ InstallSGProgram.lua
@@ -1,2 +1,1 @@
-pastebin get -f 84bZeuy7 home/dial.lua
-wget 'https://github.com/Parabellum12/Stargate-Programs/blob/main/StargateInterface.lua' home/SGInterface.lua
+wget("https://github.com/Parabellum12/Stargate-Programs/blob/main/StargateInterface.lua", "home/SGInterface.lua")
--- StargateInterface.lua
+++ StargateInterface.lua
@@ -83,3 +83,5 @@ end
 function getIrisDurability()
     return sg.getIrisDurability()
 end
+
+print("Testing")
