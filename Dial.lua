315b0f221333f5d61884b7ae64834c52850e68c3
Parabellum12 <amessmer@themessmers.net>
2024-02-20 11:26:18 -0800

    test

--- Dial.lua
+++ Dial.lua
@@ -1,6 +1,7 @@
 
 local c = require("component")
 local sg = c.stargate
+local event = require("event")
 
 local dial = {}
 
@@ -9,11 +10,19 @@ local address
 function dial.Dial(symbols) 
     addressLength = #symbols
     address = symbols
+    dial.DialNext(0)
 end
 
 function dial.DialNext(lastDialedIndex)
     local glyph = address[lastDialedIndex+1]
     sg.engageSymbol(glyph);
+end
+
+
+function dial.cancelEvents()
+	event.cancel(eventEngaged)
+	event.cancel(openEvent)
+	event.cancel(failEvent)
 end
 
 engagedEvent = event.listen("stargate_spin_chevron_engaged", function(evname, address, caller, num, lock, glyph)
@@ -26,6 +35,18 @@ engagedEvent = event.listen("stargate_spin_chevron_engaged", function(evname, ad
         dial.DialNext(num)
     end
 end)
+
+openEvent = event.listen("stargate_open", function()
+	print("Stargate opened successfully")
+	dial.cancelEvents()
+end)
+
+failEvent = event.listen("stargate_failed", function()
+	print("Stargate failed to open")
+	dial.cancelEvents()
+end)
+
+
 
 
 return dial
