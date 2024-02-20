48d99936d838b3c5c9095b6817df814ba5fae60e
Parabellum12 <amessmer@themessmers.net>
2024-02-19 16:39:47 -0800

    testing

--- InstallSGProgram.lua
+++ InstallSGProgram.lua
@@ -1,2 +1,2 @@
-shell.execute('wget -f \'https://github.com/Parabellum12/Stargate-Programs/blob/main/StargateInterface.lua\' home/SGInterface.lua')
-print("Downloading File")
+shell.execute("wget -f 'https://raw.githubusercontent.com/Parabellum12/Stargate-Programs/main/StargateInterface.lua' /home/SGInterface.lua")
+shell.execute("pastebin get -f JKVyi8H0 test.lua")
