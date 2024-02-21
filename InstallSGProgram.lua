a462f98de5b72175ffd68c597d239e30ce10906b
Parabellum12 <amessmer@themessmers.net>
2024-02-20 22:44:25 -0800

    Update InstallSGProgram.lua

--- InstallSGProgram.lua
+++ InstallSGProgram.lua
@@ -1,4 +1,4 @@
-local files = {"StargateInterface.lua", "testProgram.lua", "Dial.lua", "sgc.lua"}
+local files = {"StargateInterface.lua", "TestProgram.lua", "Dial.lua", "sgc.lua"}
 for index, value in ipairs(files) do
     os.execute("wget -f 'https://raw.githubusercontent.com/Parabellum12/Stargate-Programs/main/"..value.."' /home/"..value)
 
