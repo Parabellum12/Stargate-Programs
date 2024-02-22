4d66273656b7d919bb53b1bde8872b47c0220591
Parabellum12 <amessmer@themessmers.net>
2024-02-21 19:57:07 -0800

    Update sgc.lua

--- sgc.lua
+++ sgc.lua
@@ -18,7 +18,7 @@ function printDiagnostic()
     print("GATE TYPE: "..sgi.getSgType())
     print("GATE STATUS: "..sgi.getStatus())
     print("STORED ENERGY:  "..sgi.getStoredEnergy().." RF")
-    print("MAXIMUM ENERGY: "..sgi.getMaximumEnergy().." RF")
+    print("MAXIMUM ENERGY: "..sgi.getMaximumEnergy().." RF") --throws nil error (function doesnt exist i think) idk why
     print("CAPACITORS INSTALLED: "..sgi.getNumCapacitors())
     print("IRIS TYPE: "..sgi.getIrisType())
     print("IRIS STATUS: "..sgi.getIrisState())
